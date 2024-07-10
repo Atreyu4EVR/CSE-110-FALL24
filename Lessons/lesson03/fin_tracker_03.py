82fe4338f4b2393dcfafdd0c7cf6e92e09e7445c
Ron_Vallejo <drawl-rupiahs0n@icloud.com>
2024-07-09 19:39:14 -0600

    Added new content

---
+++ Lessons/lesson03/fin_tracker_03.py
@@ -0,0 +1,20 @@
+"""
+Lesson: 02 Input/Output
+Purpose: From user input, output the name and system messages.
+"""
+# --- DO NOT EDIT BELOW --- #
+def main():
+# --- DO NOT EDIT ABOVE --- #
+
+# ↓↓↓ Create your code below ↓↓↓ #
+
+    print("Welcome to REPLACE WITH APPLICATION NAME HERE!")
+    name = input("Provide your name here: ")
+    print(f"Hello, {name}! Let's manage your finances.")
+
+# ↑↑↑ Create your code above ↑↑↑ #
+
+# --- DO NOT EDIT BELOW --- #
+if __name__ == "__main__":
+    main()
+# --- DO NOT EDIT ABOVE --- #
