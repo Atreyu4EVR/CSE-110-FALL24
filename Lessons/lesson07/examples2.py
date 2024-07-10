8735990edb8b6f4aca4111f0bd0363d60c0e9830
Ron_Vallejo <drawl-rupiahs0n@icloud.com>
2024-07-09 19:49:52 -0600

    changed files

--- Lessons/lesson07/examples2.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-
-image_beach = Image.open("/Users/sburton/git/byui-cse/cse110-faculty/instructor_solutions/Prove08/images/cactus.jpg")
-
-image_beach.show()
--- Lessons/lesson13/prepare_samples.py
+++
@@ -1,9 +0,0 @@
-# First define the function
-def print_message():
-    print("Hello CSE 110 World!")
-
-# Call the function now
-print_message()
-
-# Call it again here
-print_message()
